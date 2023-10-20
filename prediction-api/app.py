7064a55c638986cbd03f76f02f9fd61e8a89c93f
NBillar <n.t.s.billar@tilburguniversity.edu>
2023-10-20 17:27:06 +0200

    New API folder and UI DOCKER modified

---
+++ prediction-api/app.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, request
+
+from wine_quality_predictor import WinePredictor #[IMPORTS MODEL FUNCTION]
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/wine_quality_predictor/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
+def predict_str():
+    # the prediction input data in the message body as a JSON payload
+    prediction_inout = request.get_json()
+    return dp.predict_single_record(prediction_inout)
+
+
+dp = WinePredictor()
+# The code within this conditional block will only run the python file is executed as a
+# script. See https://realpython.com/if-name-main-python/
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
