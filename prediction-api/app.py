fc6fbdf382ca4c8fb59cd35062cfffd0570b6f03
Elano Beer <e.h.u.beer@student.tue.nl>
2023-10-23 16:03:09 +0200

    improve api to app.py

--- prediction-api/app.py
+++ prediction-api/app.py
@@ -2,21 +2,22 @@ import os
 
 from flask import Flask, request
 
-from wine_quality_predictor import predict_wine_quality #[IMPORTS MODEL FUNCTION]
+from winequality_predictor import predict_wine_quality  # [IMPORTS MODEL FUNCTION]
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/wine_quality_predictor/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
+@app.route(
+    "/wine_quality_predictor/", methods=["POST"]
+)  # path of the endpoint. Except only HTTP POST request
 def predict_str():
     # the prediction input data in the message body as a JSON payload
     prediction_inout = request.get_json()
-    return dp.predict_single_record(prediction_inout)
+    return predict_wine_quality(request=request, PROJECT_ID=PROJECT_ID)
 
 
-dp = predict_wine_quality()
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
-if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(port=int(os.environ.get("PORT", 5000)), host="0.0.0.0", debug=True)
--- prediction-api/winequality_predictor.py
+++ prediction-api/winequality_predictor.py
@@ -5,42 +5,86 @@ import joblib
 import pandas as pd
 from flask import jsonify
 from google.cloud import storage
+import pickle
+import re
 
 
-def predict_wine_quality(request):
+def predict_wine_quality(request, PROJECT_ID):
     """
     This function will be executed when the endpoint is called
     """
     # extract the request body
     request_json = request.get_json()
+
     # logging the request body
     print("Request received : {}".format(request_json))
 
     # extract the request parameters
     # => Nodig?
-    fixed_acidity = request_json['fixed_acidity']
-    volatile_acidity = request_json['volatile_acidity']
-    citric_acid = request_json['citric_acid']
-    residual_sugar = request_json['residual_sugar']
-    chlorides = request_json['chlorides']
-    free_sulfur_dioxide = request_json['free_sulfur_dioxide']
-    total_sulfur_dioxide = request_json['total_sulfur_dioxide']
-    density = request_json['density']
-    pH = request_json['pH']
-    sulphates = request_json['sulphates']
-    alcohol = request_json['alcohol']
+    fixed_acidity = request_json["fixed_acidity"]
+    volatile_acidity = request_json["volatile_acidity"]
+    citric_acid = request_json["citric_acid"]
+    residual_sugar = request_json["residual_sugar"]
+    chlorides = request_json["chlorides"]
+    free_sulfur_dioxide = request_json["free_sulfur_dioxide"]
+    total_sulfur_dioxide = request_json["total_sulfur_dioxide"]
+    density = request_json["density"]
+    pH = request_json["pH"]
+    sulphates = request_json["sulphates"]
+    alcohol = request_json["alcohol"]
+
+    form_data = pd.DataFrame(
+        data={
+            "fixed acidity": fixed_acidity,
+            "volatile acidity": volatile_acidity,
+            "citric acid": citric_acid,
+            "residual sugar": residual_sugar,
+            "chlorides": chlorides,
+            "free sulfur dioxide": free_sulfur_dioxide,
+            "total sulfur dioxide": total_sulfur_dioxide,
+            "density": density,
+            "pH": pH,
+            "sulphates": sulphates,
+            "alcohol": alcohol,
+        }
+    )
 
     # load the model from cloud storage
 
-    #---------------------------------
-    ## => Waar staat de model file?
-    model = joblib.load('X')
-    #---------------------------------
+    client = storage.Client(project=PROJECT_ID)
+    blobs = [
+        (blob, blob.updated)
+        for blob in client.list_blobs(
+            "models_de2023_20204025",
+        )
+    ]
+    latest = sorted(blobs, key=lambda tup: tup[1])[-1][0]
+    latest_model = re.split("/", latest.id)[1]
+    bucket = client.bucket("models_de2023_2065718")
+    blob = bucket.blob(latest_model)
+    with blob.open(mode="rb") as f:
+        model = pickle.load(f)
+
+    blob = bucket.blob("pca_model.pkl")
+    with blob.open(mode="rb") as g:
+        pca = pickle.load(g)
+
+    blob = bucket.blob("scaler_model.pkl")
+    with blob.open(mode="rb") as h:
+        scaler = pickle.load(h)
+
+    data = pd.DataFrame(
+        scaler.transform(form_data),
+        columns=form_data.columns,
+        index=form_data.index,
+    )
+
+    X = pd.DataFrame(pca.transform(data))
 
     # make prediction
-    df = pd.read_json(json.dumps(request_json), orient='records')
-    print(df, file=sys.stdout)
-    y_pred = model.predict(df)
+    # df = pd.read_json(json.dumps(request_json), orient="records")
+    # print(df, file=sys.stdout)
+    y_pred = model.predict(X)
 
     # return the response
-    return jsonify({'result': y_pred[0].tolist()})
+    return jsonify({"result": y_pred[0].tolist()})
